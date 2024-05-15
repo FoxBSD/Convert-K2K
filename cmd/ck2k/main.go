7a0026db1f5edcd596ea5d14c7088f74e8dfec38
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2024-05-15 00:34:04 -0300

    :tada: init: adding a main file from project

--- cmd/ck2k/main.go
+++ cmd/ck2k/main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"convert-k2k/internal/db"
+	"convert-k2k/internal/debug"
+	"convert-k2k/internal/reconstruction"
+	"convert-k2k/internal/walker"
+	"database/sql"
+	"errors"
+	"flag"
+)
+
+var database *sql.DB
 
 func main() {
-	fmt.Println("Hello World!")
+	dir := flag.String("dir", "", "this flag is used to pass the BSD OS dir")
+	flag.Parse()
+
+	if *dir != "" {
+		database = db.CreateDB()
+
+		walker.WalkAndInsert(*dir, database)
+
+		repos := db.GetRepos(database)
+
+		for _, repo := range repos {
+			reconstruction.CreateNewPath(repo)
+		}
+
+		defer database.Close()
+		return
+	}
+
+	debug.PrintErrorMessage(*dir, errors.New("we need the flag -dir with BSD OS path to convert"))
 }
