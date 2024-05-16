5656d89926278d40f9fc319ca6670339827e54f4
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2024-05-16 00:33:31 -0300

    :recycle: refactoring: now we receive nbsd flag and pass the file name in parameter

--- cmd/ck2k/main.go
+++ cmd/ck2k/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"convert-k2k/internal/db"
 	"convert-k2k/internal/debug"
+	"convert-k2k/internal/files"
 	"convert-k2k/internal/reconstruction"
 	"convert-k2k/internal/walker"
 	"database/sql"
@@ -14,17 +15,19 @@ var database *sql.DB
 
 func main() {
 	dir := flag.String("dir", "", "this flag is used to pass the BSD OS dir")
+	notIsBSD := flag.Bool("nbsd", true, "this flag is used to verify if is a bsd project")
 	flag.Parse()
 
 	if *dir != "" {
-		database = db.CreateDB()
+		file := files.GetFileName(*dir)
+		database = db.CreateDB(file)
 
-		walker.WalkAndInsert(*dir, database)
+		walker.WalkAndInsert(*dir, *notIsBSD, database)
 
 		repos := db.GetRepos(database)
 
 		for _, repo := range repos {
-			reconstruction.CreateNewPath(repo)
+			reconstruction.CreateNewPath(repo, file)
 		}
 
 		defer database.Close()
