b4cfa33bba2c778d143345219e17c07b0461fb99
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2024-05-16 00:34:35 -0300

    :recycle: refactoring: now the code create a directory with name of repository

--- internal/reconstruction/createNewPath.go
+++ internal/reconstruction/createNewPath.go
@@ -3,20 +3,28 @@ package reconstruction
 import (
 	"convert-k2k/internal/debug"
 	"convert-k2k/internal/structures"
+	"fmt"
+	"path/filepath"
 	"strings"
 )
 
 // This function parses the old path in a new path
-func CreateNewPath(f structures.FileData) {
+func CreateNewPath(f structures.FileData, n string) {
 	words := strings.Split(f.FilePath, "/")
 	exts := strings.Split(f.GetExt(), ".")
 
+	reconstructed := fmt.Sprintf("reconstructed-%s", n)
+
+	if words[0] != ".." {
+		words[0] = "./"
+	}
+
 	if f.GetExt() == "" {
-		words[1] = "reconstructed/script"
+		words[1] = filepath.Join(reconstructed, "script")
 	}
 
 	if f.GetExt() != "" {
-		words[1] = "reconstructed/" + strings.ToLower(exts[1])
+		words[1] = filepath.Join(reconstructed, strings.ToLower(exts[1]))
 	}
 
 	word := strings.Join(words, "/")
